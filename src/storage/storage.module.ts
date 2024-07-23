e0579d10b5507b5578bbf9dbe89b162f24ff3042
Andrei Riaskov <code@riaskov.com>
2024-07-23 20:35:34 +0300

    fix: add services and entities to StorageModule file

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,11 +1,17 @@
 import { Module } from "@nestjs/common"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { storageProviders } from "./storage.providers"
-import { UserEntity } from "./users/users.entity"
+import { UserEntity } from "./users/user.entity"
+import { QuestEntity } from "./quests/quest.entity"
+import { QuestCompletionEntity } from "./quest-completions/quest-completion.entity"
+import { QuestsStorageService } from "./quests/quests.service"
 
 @Module({
-  imports: [...storageProviders, TypeOrmModule.forFeature([UserEntity])],
-  providers: [],
-  exports: [],
+  imports: [
+    ...storageProviders,
+    TypeOrmModule.forFeature([UserEntity, QuestEntity, QuestCompletionEntity]),
+  ],
+  providers: [QuestsStorageService],
+  exports: [QuestsStorageService],
 })
 export class StorageModule {}
