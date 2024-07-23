0342ff1ea21b446ba863b08ee361306fd46f5c08
Andrei Riaskov <code@riaskov.com>
2024-07-23 17:13:04 +0300

    feat: add main app module

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from "@nestjs/common"
+import { GraphQLModule } from "@nestjs/graphql"
+import { MercuriusDriver, MercuriusDriverConfig } from "@nestjs/mercurius"
+import { ConfigModule } from "@nestjs/config"
+import * as Joi from "joi"
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        HOST: Joi.string().default("localhost"),
+        PORT: Joi.number().default(3030),
+        POSTGRES_HOST: Joi.string().default("localhost"),
+        POSTGRES_PORT: Joi.number().default(5432),
+        POSTGRES_USER: Joi.string().default("large_dataset_api_demo"),
+        POSTGRES_PASSWORD: Joi.string().allow("").optional(),
+        POSTGRES_DB: Joi.string().default("large_dataset_api_demo"),
+      }),
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
+      debug: true,
+      graphiql: true,
+      path: "/api/v0/graphql",
+    }),
+  ],
+})
+export class AppModule {}
