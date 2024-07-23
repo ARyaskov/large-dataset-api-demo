2ed18ceed98c80bbfaf3137c1f847946180b4b68
Andrei Riaskov <code@riaskov.com>
2024-07-23 17:59:37 +0300

    devops: add init-db script for docker db

---
+++ init-db/init-db.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "Creating user and database if they do not exist..."
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  DO
+  \$do\$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$POSTGRES_USER') THEN
+      CREATE ROLE $POSTGRES_USER WITH LOGIN PASSWORD '$POSTGRES_PASSWORD';
+      GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
+    ELSE
+      RAISE NOTICE 'Role $POSTGRES_USER already exists. Skipping creation.';
+      ALTER USER $POSTGRES_USER PASSWORD '$POSTGRES_PASSWORD';
+    END IF;
+  END
+  \$do\$;
+
+  CREATE DATABASE $POSTGRES_DB OWNER $POSTGRES_USER;
+  GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
+EOSQL
+
+echo "User and database creation script completed."
