f7ea85d960ffb822a211c7b1ee5763d300ff7565
Andrei Riaskov <code@riaskov.com>
2024-07-23 21:04:45 +0300

    fix: use bigint instead of regular int in Quest storage entity

--- src/storage/quests/quest.entity.ts
+++ src/storage/quests/quest.entity.ts
@@ -3,11 +3,11 @@ import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
 @Entity()
 export class QuestEntity {
   @PrimaryGeneratedColumn()
-  id: number
+  id: bigint
 
   @Column()
   name: string
 
   @Column()
-  eth_reward: number
+  eth_reward: bigint
 }
